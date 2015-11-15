7cbcb721e3bb6bc33fa9105074bafdff730ad681
kjwitt <witt.87@osu.edu>
2015-11-14 19:11:20 -0500

    changed location

--- Libraries/FEHLCD.cpp
+++ Libraries/FEHLCD.cpp
@@ -426,7 +426,7 @@ void FEHLCD::_Initialize()
     LCD.PrintImage(111,30);
     LCD.PrintLogo(90,175);
     LCD.SetFontColor(FEHLCD::Black);
-    LCD.WriteRC("11.14.15",26,0);
+    LCD.WriteRC("11.14.15",0,0);
 
     WriteIndex(0x29);  //display on
     Sleep(100);
