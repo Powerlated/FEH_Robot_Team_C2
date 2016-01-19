1cb3e5ab7d664584f7dbbdb45544eb94e59a9a86
Max Kross <kross.8@osu.edu>
2016-01-19 12:37:31 -0500

    The drive name is now stored to the %usb% variable. So FEHSD is now hunted down instead of specified as F:\ or E:\

--- Tools/copyS19.bat
+++ Tools/copyS19.bat
@@ -1,6 +1,4 @@
-del F:\*.S19
-xcopy ..\*.s19 F:\
-move F:\*.s19 F:\CODE.S19
-del E:\*.S19
-xcopy ..\*.s19 E:\
-move E:\*.s19 E:\CODE.S19
+for /f %%D in ('wmic volume get DriveLetter^, Label ^| find "FEHSD"') do set usb=%%D
+del %usb%\*.S19
+xcopy ..\*.s19 %usb%\
+move %usb%\*.s19 %usb%\CODE.S19
