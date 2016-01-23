23ebc8565c75fb97c3ba7d40c2cbed0b155ea86d
Max Kross <kross.8@osu.edu>
2016-01-23 14:44:44 -0500

    Updated S19 windows copier tool to display useful error messages.

--- Tools/copyS19.bat
+++ Tools/copyS19.bat
@@ -1,4 +1,28 @@
 for /f %%D in ('wmic volume get DriveLetter^, Label ^| find "FEHSD"') do set usb=%%D
+
+if not exist %usb%\*.S19 goto ERROR2
 del %usb%\*.S19
+
 xcopy ..\*.s19 %usb%\
+IF %ERRORLEVEL% NEQ 0 GOTO COPY_ERROR
+echo S19 succesfully copied 1>&2
+
 move %usb%\*.s19 %usb%\CODE.S19
+IF %ERRORLEVEL% NEQ 0 GOTO RENAME_ERROR
+echo S19 succesfully renamed 1>&2
+GOTO QUIT
+
+:COPY_ERROR
+echo S19 file copy failed 1>&2
+exit 1
+
+:ERROR2
+echo SD card is not inserted 1>&2
+exit 1
+
+:RENAME_ERROR
+echo S19 file could not be renamed 1>&2
+exit 1
+
+:QUIT
+echo 
