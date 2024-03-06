16378311e85ad8235cfb73bc0daa9c2700bb01a1
Brian Jia <brian.jia04@gmail.com>
2024-03-06 15:40:05 -0500

    Replace Makefile with CMake

---
+++ toolchain.cmake
@@ -0,0 +1,30 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(TOOLCHAIN_PREFIX arm-none-eabi-)
+set(CMAKE_AR ${TOOLCHAIN_PREFIX}ar)
+set(CMAKE_ASM_COMPILER ${TOOLCHAIN_PREFIX}as)
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}g++)
+set(CMAKE_LINKER ${TOOLCHAIN_PREFIX}ld)
+set(CMAKE_OBJCOPY ${TOOLCHAIN_PREFIX}objcopy)
+set(CMAKE_RANLIB ${TOOLCHAIN_PREFIX}ranlib)
+set(CMAKE_SIZE ${TOOLCHAIN_PREFIX}size)
+set(CMAKE_STRIP ${TOOLCHAIN_PREFIX}strip)
+
+set(CMAKE_C_FLAGS "-ffunction-sections -fdata-sections -fmessage-length=0 -Wno-psabi -specs=nosys.specs \
+-Wall -Os -mcpu=cortex-m4 -mfloat-abi=soft -mthumb -g3 -gdwarf-2 -gstrict-dwarf -fpermissive ")
+set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -fno-exceptions")
+set(CMAKE_C_FLAGS_DEBUG "")
+set(CMAKE_C_FLAGS_RELEASE "")
+set(CMAKE_CXX_FLAGS_DEBUG "")
+set(CMAKE_CXX_FLAGS_RELEASE "")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+include(CheckIPOSupported)
+check_ipo_supported(RESULT supported OUTPUT error)
