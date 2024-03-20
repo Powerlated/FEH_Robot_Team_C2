2fbe24fbe4db6f929aa1379c6ee0acdf5639e324
Brian Jia <brian.jia04@gmail.com>
2024-03-20 11:20:19 -0400

    Re-enable optimizations because I fixed RCS/XBee bug

--- toolchain.cmake
+++ toolchain.cmake
@@ -12,7 +12,7 @@ set(CMAKE_RANLIB ${TOOLCHAIN_PREFIX}ranlib)
 set(CMAKE_SIZE ${TOOLCHAIN_PREFIX}size)
 set(CMAKE_STRIP ${TOOLCHAIN_PREFIX}strip)
 
-set(CMAKE_C_FLAGS "-ffunction-sections -fdata-sections -specs=nosys.specs -O0 -mcpu=cortex-m4 -mfloat-abi=soft -mthumb -g3 -gdwarf-2 -gstrict-dwarf")
+set(CMAKE_C_FLAGS "-ffunction-sections -fdata-sections -specs=nosys.specs -O3 -mcpu=cortex-m4 -mfloat-abi=soft -mthumb -g3 -gdwarf-2 -gstrict-dwarf")
 set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -fno-exceptions")
 set(CMAKE_C_FLAGS_DEBUG "")
 set(CMAKE_C_FLAGS_RELEASE "")
@@ -25,4 +25,4 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-#set(CMAKE_INTERPROCEDURAL_OPTIMIZATION true)
+set(CMAKE_INTERPROCEDURAL_OPTIMIZATION true)
